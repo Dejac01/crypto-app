ee21e99e8d43177c2ba509d4fa478f5a18ead950
deja.craft <dejacraft28@gmail.com>
2023-03-10 11:28:39 -0500

    Crypto-App

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import { BrowserRouter as Router } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-  <React.StrictMode>
+ 
     <Router>
     <App />
     </Router>
   
-  </React.StrictMode>
+ 
 );
 
